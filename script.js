e2cb16d207771b34a13548722407cb39440816d2
Viadsten <stanulevich.vg@yandex.ru>
2020-09-11 21:03:46 +0300

    add mobile index.html

--- script.js
+++ script.js
@@ -2,36 +2,22 @@ var navToggle = document.querySelector(".nav-toggle");
 var mainNav = document.querySelector(".header__nav-wrapper");
 
 
-var cachedWidth = $(window).width();
-$(window).resize(function(){
-    var newWidth = $(window).width();
-    if(newWidth !== cachedWidth){
-      $(window).resize(function() {
-        if(document.documentElement.clientWidth > 540) {
-          mainNav.style.display = "flex";
-        }
-      });
 
-      $(window).resize(function() {
-        if(document.documentElement.clientWidth < 540) {
-          mainNav.style.display = "none";
-          navToggle.classList.remove('toggle_active');
-        }
-      });
-        cachedWidth = newWidth;
-    }
+
+
+$(document).ready(function() {
+  $('.goods__minus').click(function () {
+      var $input = $(this).parent().find('input');
+      var count = parseInt($input.val()) - 1;
+      count = count < 1 ? 1 : count;
+      $input.val(count);
+      $input.change();
+      return false;
+  });
+  $('.goods__plus').click(function () {
+      var $input = $(this).parent().find('input');
+      $input.val(parseInt($input.val()) + 1);
+      $input.change();
+      return false;
+  });
 });
-
-navToggle.addEventListener('click', function(){
-  if (navToggle.classList.contains('toggle_active'))
-  {
-    $("#navigation").slideUp('300');
-    navToggle.classList.remove('toggle_active');
-  }
-  else{
-    $("#navigation").slideDown('300');
-    navToggle.classList.add('toggle_active');
-    mainNav.style.display = "flex";
-  }
-});
-
